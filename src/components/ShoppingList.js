f14998c683cd63bba707ee17e355eec735fcfa79
Frankline Were <franklinegift@gmail.com>
2024-08-19 16:06:33 +0300

    display items

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,13 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  // useEffect hook
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+    .then((r) => r.json())
+    .then((items) => setItems(items));
+  }, []);
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
