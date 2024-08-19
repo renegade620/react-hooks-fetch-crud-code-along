a7595002f6717cec18ecf453e908d0ebc9d3de5b
Frankline Were <franklinegift@gmail.com>
2024-08-19 16:36:38 +0300

    updated items toggle

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -19,6 +19,18 @@ function ShoppingList() {
     setItems([...items], newItem);
   }
 
+  // callback function
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if(item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
@@ -38,7 +50,7 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem}/>
         ))}
       </ul>
     </div>
